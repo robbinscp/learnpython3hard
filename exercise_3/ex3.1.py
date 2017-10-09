aa20185f3ebbbce37280fb897ca49db677fd4236
Chris Robbins <robbinsc@gmail.com>
2017-10-08 21:21:00 -0700

    Exercise 4 commits

---
+++ exercise_3/ex3.1.py
@@ -0,0 +1,4 @@
+print("Calc 1:", 2700 * 2)
+print("Calc 2:",  1600  * 4)
+print("Calc 3:", 9000)
+print("Total: " , (9000 + (1600 * 4) + (2700 * 2)))
---
+++ exercise_3/ex3.py
@@ -0,0 +1,35 @@
+# Count some chickens
+print("I will now count my chickens:")
+
+# Math to determine number of hens and roosters
+print("Hens", float(25 + 30 / 6))
+print("Roosters", float(100 - 25 * 3 % 4))
+
+# Let's count the eggs
+print("Now I will count the eggs:")
+
+# Extended math for eggs count
+print(float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+
+# Print question
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Print value of 3 + 2 < 5 - 7
+print(float(3 + 2 < 5 - 7))
+
+# Print math for previous statement
+print("What is 3 + 2?", float(3 + 2))
+print("What is 5 - 7?", float(5 - 7))
+
+# Print validation statement
+print("Oh, that's why it's false")
+
+# Let's do some more math
+print("How about some more.")
+
+# Print if 5 > -2 
+print("Is is greater?", 5 > -2)
+# Print if 5 >= -2
+print("Is is greater or equal?", 5 >= -2)
+# Print 5 <= -2 
+print("Is it less or equal?", 5 <= -2)
