946a27b6eec5488d2502e728bfabf2001234daf9
Chris Robbins <robbinsc@gmail.com>
2017-10-10 22:05:47 -0700

    Exercise 9 commits

---
+++ exercise_9/ex9.py
@@ -0,0 +1,17 @@
+# Here's some new strange stuff, remember to type it exactly.  
+
+# Set variables
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print days/months
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# Print long text with Python
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Eevn 4 lines if we want, or 5, or 6.
+""")
