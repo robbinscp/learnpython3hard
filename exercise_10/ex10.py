93832a5affadc43dfbbbb829d6a308bc764ac733
Chris Robbins <robbinsc@gmail.com>
2017-10-12 21:51:10 -0700

    Exercise 10 commits

---
+++ exercise_10/ex10.py
@@ -0,0 +1,17 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat  = "I'm split\non a line."
+backslash_cat  = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I"ll do a list: 
+\t* Cat food \t $5.00
+\t* Fishies \t $10.00
+\t* Catnip\t $100.00\n\t* Grass \t $99.00
+'''
+noisy_cat = "Oooga\a"
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(noisy_cat)
